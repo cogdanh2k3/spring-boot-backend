badfa45adf9cc1ba3b7f53a052bcfd553b56b7be
Nguyen Van Thong <thonghop222@gmail.com>
2025-12-06 15:05:03 +0700

    coding...

--- src/main/java/com/springboot/auth/repository/UserRepository.java
+++ src/main/java/com/springboot/auth/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.springboot.auth.repository;
 
-import com.springboot.auth.entity.User;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.springboot.auth.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
